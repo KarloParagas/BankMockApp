9c39a696f4947e9b4acaaa8dcb4643f1a7b30881
KarloParagas <karloparagas@gmail.com>
2020-03-08 21:40:39 -0700

    Created Receipt class

---
+++ BankMockApp/Receipt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankMockApp
+{
+    class Receipt
+    {
+        [Key]
+        public int ReceiptId { get; set; }
+
+        [Required]
+        public int AccountNumber { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public double Amount { get; set; }
+
+        [Required]
+        public double NewCheckingBalance { get; set; }
+
+        [Required]
+        public double NewSavingsBalance { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; }
+    }
+}
