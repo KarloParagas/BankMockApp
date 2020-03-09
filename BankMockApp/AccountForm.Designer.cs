14ef7bb9e41150a42489652873e70f1cd8dbc3c4
KarloParagas <karloparagas@gmail.com>
2020-03-08 20:26:40 -0700

    Created AccountForm

---
+++ BankMockApp/AccountForm.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace BankMockApp
+{
+    partial class AccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DeleteAccountBtn = new System.Windows.Forms.Button();
+            this.WithdrawBtn = new System.Windows.Forms.Button();
+            this.SavingsBalanceLbl = new System.Windows.Forms.Label();
+            this.CheckingBalanceLbl = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TotalBalanceLbl = new System.Windows.Forms.Label();
+            this.AccountNumLbl = new System.Windows.Forms.Label();
+            this.AccountHolderInfoLbl = new System.Windows.Forms.Label();
+            this.LogoutBtn = new System.Windows.Forms.Button();
+            this.DepositBtn = new System.Windows.Forms.Button();
+            this.TransferBtn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DeleteAccountBtn
+            // 
+            this.DeleteAccountBtn.Location = new System.Drawing.Point(411, 12);
+            this.DeleteAccountBtn.Name = "DeleteAccountBtn";
+            this.DeleteAccountBtn.Size = new System.Drawing.Size(89, 23);
+            this.DeleteAccountBtn.TabIndex = 32;
+            this.DeleteAccountBtn.Text = "Delete Account";
+            this.DeleteAccountBtn.UseVisualStyleBackColor = true;
+            // 
+            // WithdrawBtn
+            // 
+            this.WithdrawBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.WithdrawBtn.Location = new System.Drawing.Point(249, 262);
+            this.WithdrawBtn.Name = "WithdrawBtn";
+            this.WithdrawBtn.Size = new System.Drawing.Size(251, 39);
+            this.WithdrawBtn.TabIndex = 23;
+            this.WithdrawBtn.Text = "Withdraw";
+            this.WithdrawBtn.UseVisualStyleBackColor = true;
+            // 
+            // SavingsBalanceLbl
+            // 
+            this.SavingsBalanceLbl.AutoSize = true;
+            this.SavingsBalanceLbl.Location = new System.Drawing.Point(381, 105);
+            this.SavingsBalanceLbl.Name = "SavingsBalanceLbl";
+            this.SavingsBalanceLbl.Size = new System.Drawing.Size(28, 13);
+            this.SavingsBalanceLbl.TabIndex = 31;
+            this.SavingsBalanceLbl.Text = "5.05";
+            // 
+            // CheckingBalanceLbl
+            // 
+            this.CheckingBalanceLbl.AutoSize = true;
+            this.CheckingBalanceLbl.Location = new System.Drawing.Point(381, 67);
+            this.CheckingBalanceLbl.Name = "CheckingBalanceLbl";
+            this.CheckingBalanceLbl.Size = new System.Drawing.Size(28, 13);
+            this.CheckingBalanceLbl.TabIndex = 30;
+            this.CheckingBalanceLbl.Text = "5.05";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label7.Location = new System.Drawing.Point(21, 27);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 26);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "Account Info:";
+            // 
+            // TotalBalanceLbl
+            // 
+            this.TotalBalanceLbl.AutoSize = true;
+            this.TotalBalanceLbl.Location = new System.Drawing.Point(133, 139);
+            this.TotalBalanceLbl.Name = "TotalBalanceLbl";
+            this.TotalBalanceLbl.Size = new System.Drawing.Size(40, 13);
+            this.TotalBalanceLbl.TabIndex = 28;
+            this.TotalBalanceLbl.Text = "$10.10";
+            // 
+            // AccountNumLbl
+            // 
+            this.AccountNumLbl.AutoSize = true;
+            this.AccountNumLbl.Location = new System.Drawing.Point(133, 105);
+            this.AccountNumLbl.Name = "AccountNumLbl";
+            this.AccountNumLbl.Size = new System.Drawing.Size(31, 13);
+            this.AccountNumLbl.TabIndex = 27;
+            this.AccountNumLbl.Text = "1111";
+            // 
+            // AccountHolderInfoLbl
+            // 
+            this.AccountHolderInfoLbl.AutoSize = true;
+            this.AccountHolderInfoLbl.Location = new System.Drawing.Point(133, 67);
+            this.AccountHolderInfoLbl.Name = "AccountHolderInfoLbl";
+            this.AccountHolderInfoLbl.Size = new System.Drawing.Size(59, 13);
+            this.AccountHolderInfoLbl.TabIndex = 26;
+            this.AccountHolderInfoLbl.Text = "John Smith";
+            // 
+            // LogoutBtn
+            // 
+            this.LogoutBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.LogoutBtn.Location = new System.Drawing.Point(26, 208);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(81, 39);
+            this.LogoutBtn.TabIndex = 16;
+            this.LogoutBtn.Text = "Log Out";
+            this.LogoutBtn.UseVisualStyleBackColor = true;
+            // 
+            // DepositBtn
+            // 
+            this.DepositBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.DepositBtn.Location = new System.Drawing.Point(249, 154);
+            this.DepositBtn.Name = "DepositBtn";
+            this.DepositBtn.Size = new System.Drawing.Size(251, 39);
+            this.DepositBtn.TabIndex = 19;
+            this.DepositBtn.Text = "Deposit";
+            this.DepositBtn.UseVisualStyleBackColor = true;
+            // 
+            // TransferBtn
+            // 
+            this.TransferBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.TransferBtn.Location = new System.Drawing.Point(249, 208);
+            this.TransferBtn.Name = "TransferBtn";
+            this.TransferBtn.Size = new System.Drawing.Size(251, 39);
+            this.TransferBtn.TabIndex = 20;
+            this.TransferBtn.Text = "Transfer";
+            this.TransferBtn.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label6.Location = new System.Drawing.Point(244, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 26);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Accounts:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Savings Balance:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Checking Balance:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Total Balance:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Account Number:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Account Holder:";
+            // 
+            // AccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 319);
+            this.Controls.Add(this.DeleteAccountBtn);
+            this.Controls.Add(this.WithdrawBtn);
+            this.Controls.Add(this.SavingsBalanceLbl);
+            this.Controls.Add(this.CheckingBalanceLbl);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TotalBalanceLbl);
+            this.Controls.Add(this.AccountNumLbl);
+            this.Controls.Add(this.AccountHolderInfoLbl);
+            this.Controls.Add(this.LogoutBtn);
+            this.Controls.Add(this.DepositBtn);
+            this.Controls.Add(this.TransferBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AccountForm";
+            this.Text = "AccountForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DeleteAccountBtn;
+        private System.Windows.Forms.Button WithdrawBtn;
+        private System.Windows.Forms.Label SavingsBalanceLbl;
+        private System.Windows.Forms.Label CheckingBalanceLbl;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label TotalBalanceLbl;
+        private System.Windows.Forms.Label AccountNumLbl;
+        private System.Windows.Forms.Label AccountHolderInfoLbl;
+        private System.Windows.Forms.Button LogoutBtn;
+        private System.Windows.Forms.Button DepositBtn;
+        private System.Windows.Forms.Button TransferBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
